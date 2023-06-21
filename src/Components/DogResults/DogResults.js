79cbda4d16b0a8c748a93dc9363326a62368ebc9
CJ484 <ccoreas484@gmail.com>
2023-06-21 11:48:47 -0400

    API has been activated and delivering results

--- src/Components/DogResults/DogResults.js
+++ src/Components/DogResults/DogResults.js
@@ -1,9 +1,34 @@
+import axios from "axios";
+
+import './DogResults.styles.scss'
 import pic from "../../assets/dogInCage.jpg";
+import { useEffect } from "react";
 function DogResults() {
+
+  useEffect(() => {
+      results();
+  })
+
+  const holdThis = process.env.REACT_APP_RUFF_RUFF
+  const headers = {
+    'X-Api-Key': holdThis,
+  }
+  const name = 'golden retriever';
+
+  async function results() {
+    await axios
+    .get(`https://api.api-ninjas.com/v1/dogs?name=${name}`, {
+    headers: headers,
+    contentType: 'application/json',
+  })
+  .then((results) => {
+    console.log(results);
+  })
+  } 
   return (
-    <div>
+    <div className="dogResults">
       <img src={pic} alt="example" />
-    </div>
+   </div>
   );
 }
 
