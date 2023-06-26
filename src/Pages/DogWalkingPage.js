0e7ccebf2d32fb37203fb91f569dd98a5ca60edc
CJ-Codes <ccoreas484@gmail.com>
2023-06-26 15:36:08 -0400

    removal unnessary lines

--- src/Pages/DogWalkingPage.js
+++ src/Pages/DogWalkingPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./DogResults/DogResultsPage.scss";
-import { useSelector } from "react-redux";
 import Banner from "../Components/Banner/Banner";
 import FilterResults from "../Components/FilterResults/FilterResults.js";
 import DisplayFilters from "../Components/DisplayFilters/DisplayFilters";
@@ -8,7 +7,6 @@ import DogCardInfo from "../Components/CreateDog/CreateDog";
 import PaginationFunction from "../Components/Pagination/Pagination.js";
 
 const DogWalkingPage = () => {
-  const dogs = useSelector((state) => state.dogs);
 
   return (
     <div className="test">
