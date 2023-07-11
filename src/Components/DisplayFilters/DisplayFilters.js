fae4e44a51b349cfaaecad04f5e7fe5907a11c3f
CJ-Codes <ccoreas484@gmail.com>
2023-07-10 23:58:31 -0400

    is now able to display filters on radio box change

--- src/Components/DisplayFilters/DisplayFilters.js
+++ src/Components/DisplayFilters/DisplayFilters.js
@@ -1,15 +1,41 @@
-import './DisplayFilters.styles.scss'
+import "./DisplayFilters.styles.scss";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { removeSelected } from "../../Redux/Filters/DisplayFilterRedux";
+import { setFilter } from "../../Redux/Filters/FilterRedux";
 
 const DisplayFilters = () => {
- 
+  const dispatch = useDispatch();
+  const selectedFilters = useSelector((state) => state.reducer.displayFilters.filterList);
+  const [filterNames, setFilterNames] = useState([]);
 
-  return (
-    <div className="displayFilters">
-      <h5>DisplayFilters</h5>
-    </div>
-  );
-}
+  const removeSelectedFilter = (input, name) => {
+    const noValue = 0;
+    dispatch(setFilter({key: name, noValue}));
+    dispatch(removeSelected(input));
+  }
 
+  useEffect(() => {
+    setFilterNames(selectedFilters);
+  }, [dispatch, filterNames, setFilterNames, selectedFilters])
 
+  if(filterNames.length === 0) {
+    return <div></div>
+  } else {
+    return (
+      <div className="displayFilters">
+        {filterNames.map((name) => (
+          <div className="selectedFilters">
+            <FontAwesomeIcon  className="fa-lg" icon={faCircleXmark} onClick={() => removeSelectedFilter(name)}/>
+            <h6 key={name}>{name}</h6>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+};
 
 export default DisplayFilters;
--- src/Components/FilterResults/FilterResults.js
+++ src/Components/FilterResults/FilterResults.js
@@ -1,59 +1,82 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./FilterResults.styles.scss";
 import { Form } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
+import { createList } from "../../Redux/Filters/DisplayFilterRedux";
 import { setFilter } from "../../Redux/Filters/FilterRedux";
 
 const FilterComponent = () => {
   const { t } = useTranslation();
-  const filters = useState({});
   const dispatch = useDispatch();
+  const [filterNames, setFilterNames] = useState([]);
 
-  const handleFilterChange = (event) => {
-    const {name, value} = event.target;
+  useEffect(() => {
+    dispatch(createList(filterNames));
+  }, [dispatch, filterNames]);
+
+  const handleResultSelection = (result, name, value) => {
+    const updatedResults = filterNames.includes(result)
+      ? filterNames.filter((selectedResult) => selectedResult !== result)
+      : [...filterNames, result];
+    dispatch(setFilter(name, value));
+    setFilterNames(updatedResults);
+  };
+
+  const handleCheckboxChange = (event, result) => {
+    const {name, value} = event.target
+    if (event.target.checked) {
+      handleResultSelection(result, name, value);
+    } else {
+      const updatedResults = filterNames.filter(
+        (selectedResult) => selectedResult !== result
+      );
+      setFilterNames(updatedResults);
+    }
     dispatch(setFilter({key: name, value}));
-  }
+  };
 
   return (
     <div className="filterSection">
       <Form className="filter-component">
-        <h5>{t('filters.title')}</h5>
+        <h5>
+          <b>{t("filters.title")}</b>
+        </h5>
         <Form.Check
           type="checkbox"
           id="A"
           name="trainability"
-          value={5}
-          label={t('filters.option-1')}
-          checked={filters.A}
-          onChange={handleFilterChange}
+          value="5"
+          label={t("filters.option-1")}
+          checked={filterNames.includes(t("filters.option-1"))}
+          onChange={(e) => handleCheckboxChange(e, t("filters.option-1"))}
         />
         <Form.Check
           type="checkbox"
           id="B"
           name="shedding"
-          value={5}
-          label={t('filters.option-2')}
-          checked={filters.B}
-          onChange={handleFilterChange}
+          value="5"
+          label={t("filters.option-2")}
+          checked={filterNames.includes(t("filters.option-2"))}
+          onChange={(e) => handleCheckboxChange(e, t("filters.option-2"))}
         />
         <Form.Check
           type="checkbox"
           id="C"
           name="goodWithChildren"
-          value={5}
-          label={t('filters.option-3')}
-          checked={filters.C}
-          onChange={handleFilterChange}
+          value="5"
+          label={t("filters.option-3")}
+          checked={filterNames.includes(t("filters.option-3"))}
+          onChange={(e) => handleCheckboxChange(e, t("filters.option-3"))}
         />
         <Form.Check
           type="checkbox"
           id="D"
           name="barking"
           value={5}
-          label={t('filters.option-4')}
-          checked={filters.D}
-          onChange={handleFilterChange}
+          label={t("filters.option-4")}
+          checked={filterNames.includes(t("filters.option-4"))}
+          onChange={(e) => handleCheckboxChange(e, t("filters.option-4"))}
         />
       </Form>
     </div>
