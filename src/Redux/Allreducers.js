96637cefa13fbfc977505edda13c8fc43f3977d3
CJ-Codes <ccoreas484@gmail.com>
2023-07-10 23:36:11 -0400

    updated store for dogresults, displayfilter

--- src/Redux/Allreducers.js
+++ src/Redux/Allreducers.js
@@ -3,11 +3,15 @@ import { combineReducers } from "redux";
 import loadingReducer from "./Loading/LoadingSlice.js";
 import userReducer from './User/userSlice.js';
 import filterReducer from './Filters/FilterRedux.js';
+import displayFiltersReducer from './Filters/DisplayFilterRedux.js';
+import dogResultsReducer from './DogResults/DogResultsRedux.js'
 
 const rootReducer = combineReducers({
   loading: loadingReducer,
   user: userReducer,
   filterDogs: filterReducer,
+  displayFilters: displayFiltersReducer,
+  dogResults: dogResultsReducer,
   // Add other reducers here if you have any
 });
 
