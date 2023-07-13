b061d79d3be743ce39df74809aa933dbcce9a761
CJ-Codes <ccoreas484@gmail.com>
2023-07-13 17:33:40 -0400

    deconstructed data values

--- src/Components/DogCardTemplate/DogCardTemplate.js
+++ src/Components/DogCardTemplate/DogCardTemplate.js
@@ -7,24 +7,26 @@ const CardTemplate = ({ combinedDogData }) => {
 
   return (
     <div className="cardGrid">
-      {combinedDogData.map((value, index) => (
+      {combinedDogData.map((data, index) => {
+        const { name, nameDog, trainability, image_link } = data;
+        return (
         <div key={index} className="Card">
           <div className="col-md-4">
             <div className="card">
-              <img src={value.image_link} className="card-img-top" alt="Dog" />
+              <img src={image_link} className="card-img-top" alt="Dog" />
               <div className="card-body">
                 <h6 className="label">{t("card.name")}:</h6>
-                <h5 className="card-title">{value.nameDog}</h5>
+                <h5 className="card-title">{nameDog}</h5>
                 <h6 className="label">{t("card.handling")}:</h6>
-                <h5 className="card-title">{value.trainability}</h5>
+                <h5 className="card-title">{trainability}</h5>
                 <h6 className="label">{t("card.breed")}:</h6>
-                <h5 className="card-title">{value.name}</h5>
+                <h5 className="card-title">{name}</h5>
                 <DogPenAddButton index={index}/>
               </div> 
             </div>
           </div>
         </div>
-      ))}
+      )})}
     </div>
   );
 };
