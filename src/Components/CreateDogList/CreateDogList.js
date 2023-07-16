64d4f7a760c0fe092767e9ccdda6867d180fdefc
CJ-Codes <ccoreas484@gmail.com>
2023-07-15 21:55:47 -0400

    moved api calls, made the code better to read

--- src/Components/CreateDogList/CreateDogList.js
+++ src/Components/CreateDogList/CreateDogList.js
@@ -1,93 +1,80 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import "./CreateDogList.styles.scss";
 import { useEffect, useState } from "react";
 import { connect, useDispatch } from "react-redux";
-import "./CreateDogList.styles.scss";
-import axios from "axios";
-import { setLoading } from "../../Redux/Loading/LoadingSlice";
 import Loading from "../Loading/Loading";
 import InfiniteScroll from "react-infinite-scroll-component";
 import CardTemplate from "../DogCardTemplate/DogCardTemplate";
-import urls from "../../const/urls";
+import { setLoading } from "../../Redux/Loading/LoadingSlice";
+import { setDataDog } from "../../Redux/DogResults/DogResultsRedux";
+import { apiDogCall } from "../../api/dogs/ApiCall";
+import { apiNameCall } from "../../api/name/apiCall";
 
-const CreateDogList = ({ isLoading, setLoading }) => {
+const CreateDogList = ({ isLoading, setLoading, setDataDog }) => {
+  const dispatch = useDispatch();
+  const setLoadingTrue = () => { dispatch(setLoading(true)); };
   const [dogNames, setDogNames] = useState([]);
   const [dogInfos, setDogInfos] = useState([]);
   const [combinedDogData, setCombinedDogData] = useState([]);
-  const dispatch = useDispatch();
-  const setLoadingTrue = () => { dispatch(setLoading(true)); };
-  const hasMore = useState(true);
   const [offsetAmount, setOffsetAmount] = useState(0);
   const limitperPage = 20;
-  const dogUrl = `${urls.dogNinjaApi}+&offset=${offsetAmount}`;
-  const nameUrl = urls.nameApi;
 
   useEffect(() => {
     setLoadingTrue();
-    RandomDogInfo();
-  }, [setDogInfos]);
-
+    randomDogInfo();
+  }, []);
+  
   useEffect(() => {
-    RandomName();
-  }, [setDogNames]);
+    randomNameInfo();
+  }, []);
 
-  useEffect(() => {
-    ModifiedObject(dogNames, dogInfos);
-  }, [setCombinedDogData, dogInfos]);
+  //* This function is responsible for getting image, breed Name, size into the cards
+  //* However it also saves the rest of the unused data. Set for 20 results per request
+  const randomDogInfo = async () => {
+    await apiDogCall({offsetAmount})
+    .then((results) => {
+      setDogInfos((prevDogs) => [...prevDogs, ...results]);
+      setOffsetAmount((offset) => offset + limitperPage);
+      mergeData(dogNames, dogInfos);
+    })
+  }
 
-  //This function is responsible for getting image, breed Name, size into the cards
-  //However it also saves the rest of the unused data.
-  const RandomDogInfo = async () => {
-    const key = process.env.REACT_APP_DOG;
-    await axios
-      .get(dogUrl, {
-        headers: { "X-Api-Key": key },
-      })
+  //* Requests a random name, its currently set for 50 names
+  const randomNameInfo = async () => {
+    await apiNameCall()
       .then((results) => {
-        setDogInfos((prevDogs) => [...prevDogs, ...results.data]);
-        setOffsetAmount((offset) => offset + limitperPage);
-      })
-      .catch((error) => {
-        if (error.response?.status === 404) {
-          console.error(`could not find url`, error.data);
-          return undefined;
-        }
+        setDogNames(results);
       });
   };
 
-  //Requests a random name, its currently on a loop for 12 times
-  const RandomName = async () => {
-    await axios
-      .get(nameUrl)
-      .then((results) => {
-        setDogNames(results.data.results.map((a) => a.name.first));
-      });
-  };
-
-  const ModifiedObject = (names, infos) => {
+  //* After both Requests havea been completed, we merge them both into a 
+  //* singular object
+  const mergeData = (names, infos) => {
     const combinedList = infos.map((dg) => {
       const Namey = names[Math.floor(Math.random() * names.length)];
       return { ...dg, nameDog: Namey };
     });
     setCombinedDogData(combinedList);
+    dispatch(setDataDog(combinedList))
   };
 
   if (isLoading === true) {
     return <Loading />;
-  } else {
-    return (
-      <div>
-        <InfiniteScroll
-          next={RandomDogInfo}
-          loader={<Loading />}
-          hasMore={hasMore}
-          dataLength={dogInfos.length}
-          endMessage={<p style={{ textAlign: "center" }}>End of Data</p>}
-        >
-          <CardTemplate combinedDogData={combinedDogData} />
-        </InfiniteScroll>
-      </div>
-    );
-  }
+  } 
+
+  return (
+    <div>
+      <InfiniteScroll
+        next={randomDogInfo}
+        loader={<Loading />}
+        hasMore={true}
+        dataLength={dogInfos.length}
+      >
+        <CardTemplate combinedDogData={combinedDogData} />
+      </InfiniteScroll>
+    </div>
+  );
+
 };
 
 const mapStateToProps = (state) => {
@@ -96,4 +83,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { setLoading })(CreateDogList);
+export default connect(mapStateToProps, { setLoading, setDataDog })(CreateDogList);
