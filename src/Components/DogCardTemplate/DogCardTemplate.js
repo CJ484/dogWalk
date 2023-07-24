5dd6d416e44ad35df217ed4777b97e916b9034bb
CJ-Codes <ccoreas484@gmail.com>
2023-07-23 21:43:15 -0400

    completed test for dogCardTemplate

--- src/Components/DogCardTemplate/DogCardTemplate.js
+++ src/Components/DogCardTemplate/DogCardTemplate.js
@@ -1,34 +1,62 @@
-import { useTranslation } from "react-i18next";
-import "./DogCardTemplate.styles.scss";
-import DogPenAddButton from "../DogPenAddButton/DogPenAddButton";
+/* eslint-disable camelcase */
+import React, { useId } from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import './DogCardTemplate.styles.scss';
+import DogPenAddButton from '../DogPenAddButton/DogPenAddButton';
 
-const CardTemplate = ({ combinedDogData }) => {
+function CardTemplate({ combinedDogData }) {
   const { t } = useTranslation();
-
+  // eslint-disable-next-line no-console
+  console.log(combinedDogData);
   return (
     <div className="cardGrid">
       {combinedDogData.map((data, index) => {
-        const { name, nameDog, trainability, image_link } = data;
+        const cardId = useId();
+        const {
+          name, nameDog, trainability, image_link,
+        } = data;
         return (
-        <div key={index} className="Card">
-          <div className="col-md-4">
-            <div className="card">
-              <img src={image_link} className="card-img-top" alt="Dog" />
-              <div className="card-body">
-                <h6 className="label">{t("card.name")}:</h6>
-                <h5 className="card-title">{nameDog}</h5>
-                <h6 className="label">{t("card.handling")}:</h6>
-                <h5 className="card-title">{trainability}</h5>
-                <h6 className="label">{t("card.breed")}:</h6>
-                <h5 className="card-title">{name}</h5>
-                <DogPenAddButton index={index}/>
-              </div> 
+          <div key={cardId} className="Card">
+            <div className="col-md-4">
+              <div className="card">
+                <img src={image_link} className="card-img-top" alt="Dog" />
+                <div className="card-body">
+                  <h6 className="label">
+                    {t('card.name')}
+                    {': '}
+                  </h6>
+                  <h5 className="card-title">{nameDog}</h5>
+                  <h6 className="label">
+                    {t('card.handling')}
+                    {': '}
+                  </h6>
+                  <h5 className="card-title">{trainability}</h5>
+                  <h6 className="label">
+                    {t('card.breed')}
+                    {': '}
+                  </h6>
+                  <h5 className="card-title">{name}</h5>
+                  <DogPenAddButton index={index} />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      )})}
+        );
+      })}
     </div>
   );
+}
+
+CardTemplate.propTypes = {
+  combinedDogData: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      nameDog: PropTypes.string.isRequired,
+      trainability: PropTypes.number.isRequired,
+      image_link: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default CardTemplate;
---
+++ src/Components/DogCardTemplate/DogCardTemplate.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../../Redux/store/store';
+import CardTemplate from './DogCardTemplate';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: jest.fn().mockReturnValue({ t: (key) => key }),
+}));
+
+describe('CardTemplate', () => {
+  test('renders the card template with correct data', () => {
+    const combinedDogData = [
+      {
+        name: 'Bulldog',
+        nameDog: 'Max',
+        trainability: 5,
+        image_link: 'bulldog-image-link',
+      },
+      {
+        name: 'Labrador',
+        nameDog: 'Bella',
+        trainability: 5,
+        image_link: 'labrador-image-link',
+      },
+    ];
+
+    const component = TestRenderer.create(
+      <Provider store={store}>
+        <CardTemplate combinedDogData={combinedDogData} />
+      </Provider>,
+    );
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders the card template with correct labels', () => {
+    const combinedDogData = [
+      {
+        name: 'Bulldog',
+        nameDog: 'Max',
+        trainability: 5,
+        image_link: 'bulldog-image-link',
+      },
+    ];
+
+    const component = TestRenderer.create(
+      <Provider store={store}>
+        <CardTemplate combinedDogData={combinedDogData} />
+      </Provider>,
+    );
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
