803395f30ebe20d08f744b1168baf86371766ccc
CJ-Codes <ccoreas484@gmail.com>
2023-08-10 23:14:15 -0400

    renamed, merged some redux to dog Redux

--- src/Redux/Allreducers.js
+++ src/Redux/Allreducers.js
@@ -1,23 +1,12 @@
 import { combineReducers } from 'redux';
-import loadingReducer from './Loading/LoadingSlice';
 import userReducer from './User/userSlice';
-import filterReducer from './Filters/FilterRedux';
-import displayFiltersReducer from './Filters/DisplayFilterRedux';
-import dogResultsReducer from './DogResults/DogResultsRedux';
+import dogReducer from './Dog/DogResultsRedux';
 import nameReducers from './Names/NamesRedux';
-import mergeReducers from './MergeApi/mergeApiRedux';
-import UrlConstructReducers from './UrlConstruct/UrlConstrucRedux';
 
 const rootReducer = combineReducers({
-  loading: loadingReducer,
-  urlConst: UrlConstructReducers,
   user: userReducer,
-  filterDogs: filterReducer,
-  displayFilters: displayFiltersReducer,
-  mergedApi: mergeReducers,
   names: nameReducers,
-  dogResults: dogResultsReducer,
-  // Add other reducers here if you have any
+  dog: dogReducer,
 });
 
 export default rootReducer;
