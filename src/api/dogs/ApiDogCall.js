a390d22c81f29b1ffa698b33a240845dac60d407
CJ-Codes <ccoreas484@gmail.com>
2023-08-10 23:30:51 -0400

    changed arrow body style

--- src/api/dogs/ApiDogCall.js
+++ src/api/dogs/ApiDogCall.js
@@ -4,15 +4,17 @@ import urls from '../../const/urls';
 const apiDogCall = async ({ offsetAmount, parameters }) => {
   const dogApi = urls.dogNinjaApi;
   const key = process.env.REACT_APP_DOG;
+
   const activeParameters = `${dogApi}?min_height=4&offset=${offsetAmount}&${parameters}`;
   const emptyParameters = `${dogApi}?min_height=4&offset=${offsetAmount}`;
+
   const urlRequest = parameters === undefined ? emptyParameters : activeParameters;
+
   return axios
     .get(urlRequest, {
       headers: { 'X-Api-Key': key },
     })
-    // eslint-disable-next-line arrow-body-style
-    .then((results) => { return results.data; })
+    .then((results) => results.data)
     // eslint-disable-next-line no-console
     .catch(() => console.error('could not help with that request'));
 };
