63c0989f536932e0c764fc9924f883c86dee7cd0
CJ-Codes <ccoreas484@gmail.com>
2023-09-03 21:15:04 -0400

    combined 2 functions into 1 if statement function

--- src/api/dogs/helpers.js
+++ src/api/dogs/helpers.js
@@ -1,30 +1,19 @@
 import { isEqual } from 'lodash';
 
-const formatDogApiParams = ({ filterValues }) => {
-  let lastUrl = '';
-  const dogsRedux = filterValues;
+const ACTIVE_FILTER = 5;
+
+const formatDogApiParams = ({ filters }) => {
   const newParameter = new URLSearchParams();
 
-  const addParams = () => {
-    Object.keys(dogsRedux).forEach((key) => {
-      if (isEqual(dogsRedux[key], 5)) {
-        newParameter.append(key, dogsRedux[key]);
-      }
-    });
-  };
+  Object.keys(filters).forEach((key) => {
+    if (isEqual(filters[key], ACTIVE_FILTER)) {
+      newParameter.append(key, filters[key]);
+    } else {
+      newParameter.delete(key);
+    }
+  });
 
-  const removeParams = () => {
-    Object.keys(dogsRedux).forEach((key) => {
-      if (isEqual(dogsRedux[key], 0)) {
-        newParameter.delete(key);
-      }
-    });
-  };
-
-  addParams();
-  removeParams();
-  lastUrl = newParameter.toString();
-  return lastUrl;
+  return newParameter.toString();
 };
 
 export default formatDogApiParams;
