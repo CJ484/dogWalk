be39f8178b025ddb9076192640271c70d5992501
CJ-Codes <ccoreas484@gmail.com>
2023-09-17 16:42:01 -0400

    added index file for component folder

---
+++ src/Components/index.js
@@ -0,0 +1,16 @@
+import Banner from './Banner';
+import CreateDogList from './CreateDogList';
+import CurrentProfile from './CurrentProfile';
+import DisplayFilters from './DisplayFilters';
+import DogCardTemplateList from './DogCardTemplateList';
+import DogPenAddButton from './DogPenAddButton';
+import DogPenNavButton from './DogPenNavButton';
+import FilterResults from './FilterResults';
+import LanguageList from './LanguageList';
+import Loading from './Loading';
+import NewProfile from './NewProfile';
+
+export {
+  Banner, CreateDogList, CurrentProfile, DisplayFilters, DogCardTemplateList,
+  DogPenAddButton, DogPenNavButton, FilterResults, LanguageList, Loading, NewProfile,
+};
