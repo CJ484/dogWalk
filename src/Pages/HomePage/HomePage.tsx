91c8c6bb184151bad42f4027a27aa141df07700f
Carlos Coreas <ccoreas484@gmail.com>
2026-02-02 01:05:34 -0500

    refactor: restructure HomePage layout and styles for improved organization and responsiveness, including updated class names and enhanced visual elements

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,30 +1,26 @@
 import { useTranslation } from 'react-i18next';
-import { FaChevronDown } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './HomePage.scss';
 
 function Home() {
   const { t } = useTranslation();
+
   return (
-    <div className="HomePage">
-      <div className="MainBanner">
-        <h1>{t('home.banner')}</h1>
-        <div className="twoButtons">
-          <Link to="/Pages/DogResults/DogResultPage">
-            <button type="button">
-              <h2>{t('home.bannerButton')}</h2>
-            </button>
-          </Link>
-          <FaChevronDown />
-        </div>
+    <div className="homePage">
+      <div className="homePage__banner">
+        <img className="homePage__banner__image" src='/images/happyFamily.jpg' alt="nothing yet" />
+        <h1 className="homePage__banner__title">{t('home.banner')}</h1>
+        <Link className="homePage__banner__button" to="/explore-dogs">
+          <span className="homePage__banner__button__text">{t('home.bannerButton')}</span>
+        </Link>
       </div>
-      <div className="FirstSection" id="FirstSection">
-        <img src='/images/dogHug.jpg' alt="nothing yet" />
-        <p>{t('home.paragraph-1')}</p>
+      <div className="homePage__section">
+        <img className="homePage__section__image" src='/images/huggingDog.jpg' alt="nothing yet" />
+        <p className="homePage__section__paragraph">{t('home.paragraph-1')}</p>
       </div>
-      <div className="SecondSection">
-        <p>{t('home.paragraph-2')}</p>
-        <img src='/images/cageDog.jpg' alt="nothing yet" />
+      <div className="homePage__section">
+        <p className="homePage__section__paragraph">{t('home.paragraph-2')}</p>
+        <img className="homePage__section__image" src='/images/dogInCage.jpg' alt="nothing yet" />
       </div>
     </div>
   );
