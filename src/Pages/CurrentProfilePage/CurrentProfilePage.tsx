aaa19b76b3060afcce1246009df8782b61f37493
Carlos Coreas <ccoreas484@gmail.com>
2026-02-02 01:45:10 -0500

    refactor: redesign CurrentProfilePage layout and styles for improved organization and user experience, including new animations and responsive design elements

--- src/Pages/CurrentProfilePage/CurrentProfilePage.tsx
+++ src/Pages/CurrentProfilePage/CurrentProfilePage.tsx
@@ -1,49 +1,73 @@
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import { getAllUserInfo } from '@/Redux/selectors/user';
 import { RootState } from '@/Redux/MiddleWare/index';
+import { Button } from '@/Components/ui';
 import './CurrentProfilePage.scss';
 
-function CurrentProfilePage() {
+export default function CurrentProfilePage() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const profileInfo = useSelector((state: RootState) => getAllUserInfo(state));
+
+  const handleEditProfile = () => {
+    navigate('/profile');
+  };
+
   return (
-    <div className="Page">
-      <div className="currentProfile">
-        <h2>
-          {t('currentProfile.title')}
-          :
-        </h2>
-        <div className="profileBox">
-          <div className="profileText">
-            <h4>
-              <b>
-                {t('currentProfile.user')}
-                :
-              </b>
-            </h4>
-            <h4>
-              <b>
-                {t('currentProfile.email')}
-                :
-              </b>
-            </h4>
-            <h4>
-              <b>
-                {t('currentProfile.phone')}
-                :
-              </b>
-            </h4>
+    <div className="page">
+      <div className="currentProfile__container">
+        <div className="currentProfile__header">
+          <div className="currentProfile__header__avatar">
+            <img 
+              src="/images/profile.png" 
+              alt={`${profileInfo.username}'s profile`}
+            />
           </div>
-          <div className="profileInfo">
-            <h4>{profileInfo.username}</h4>
-            <h4>{profileInfo.email}</h4>
-            <h4>{profileInfo.phoneNumber}</h4>
+          <h2 className="currentProfile__header__title">{t('currentProfile.title')}</h2>
+        </div>
+
+        <div className="currentProfile__content">
+          <div className="currentProfile__content__section">
+            <div className="currentProfile__content__section__field">
+              <label className="currentProfile__content__section__field__label">
+                {t('currentProfile.user')}
+              </label>
+              <div className="currentProfile__content__section__field__value">
+                {profileInfo.username || t('profile.user')}
+              </div>
+            </div>
+
+            <div className="currentProfile__content__section__field">
+              <label className="currentProfile__content__section__field__label">
+                {t('currentProfile.email')}
+              </label>
+              <div className="currentProfile__content__section__field__value">
+                {profileInfo.email || 'N/A'}
+              </div>
+            </div>
+
+            <div className="currentProfile__content__section__field">
+              <label className="currentProfile__content__section__field__label">
+                {t('currentProfile.phone')}
+              </label>
+              <div className="currentProfile__content__section__field__value">
+                {profileInfo.phoneNumber || 'N/A'}
+              </div>
+            </div>
+          </div>
+
+          <div className="currentProfile__content__actions">
+            <Button 
+              onClick={handleEditProfile}
+              className="currentProfile__content__actions__button"
+            >
+              {t('profile.button')}
+            </Button>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-export default CurrentProfilePage;
